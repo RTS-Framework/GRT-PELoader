60089b611baecf3ae45f0735dffc80f90b6a7c7c
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-08-14 13:22:15 +0800

    gen_args.go: adjust the order of standard handle, improve cmdline.

--- script/gen_args.go
+++ script/gen_args.go
@@ -2,31 +2,30 @@ package main
 
 import (
 	"bytes"
-	"encoding/binary"
 	"encoding/hex"
 	"fmt"
 	"os"
-	"unicode/utf16"
 
 	"github.com/RTS-Framework/GRT-Develop/argument"
+	"github.com/RTS-Framework/GRT-Develop/types"
 )
 
 func main() {
-	cmdlineA := "test_x86.exe -arg 11\x00"
-	cmdlineW := stringToUTF16(cmdlineA)
-
+	cmdline := "test_x86.exe -arg 11"
+	cmdlineA := []byte(cmdline + "\x00")
+	cmdlineW := types.StringToUTF16(cmdline)
 	args := []*argument.Arg{
-		{ID: 1, Data: []byte{0xFF}},     // invalid PE image
-		{ID: 2, Data: []byte(cmdlineA)}, // command line ANSI
-		{ID: 3, Data: []byte(cmdlineW)}, // command line UTF16
-		{ID: 4, Data: make([]byte, 4)},  // standard input handle
-		{ID: 5, Data: make([]byte, 4)},  // standard output handle
-		{ID: 6, Data: make([]byte, 4)},  // standard error handle
-		{ID: 7, Data: []byte{0x01}},     // wait main thread
-		{ID: 8, Data: []byte{0x01}},     // allow skip dll
-		{ID: 9, Data: []byte{0x01}},     // ignore standard handle
-		{ID: 10, Data: []byte{0x01}},    // not auto run
-		{ID: 11, Data: []byte{0x01}},    // not stop runtime
+		{ID: 1, Data: []byte{0xFF}},    // invalid PE image
+		{ID: 2, Data: cmdlineA},        // command line ANSI
+		{ID: 3, Data: cmdlineW},        // command line UTF16
+		{ID: 4, Data: make([]byte, 4)}, // standard input handle
+		{ID: 5, Data: make([]byte, 4)}, // standard output handle
+		{ID: 6, Data: make([]byte, 4)}, // standard error handle
+		{ID: 7, Data: []byte{0x01}},    // wait main thread
+		{ID: 8, Data: []byte{0x01}},    // allow skip dll
+		{ID: 9, Data: []byte{0x01}},    // ignore standard handle
+		{ID: 10, Data: []byte{0x01}},   // not auto run
+		{ID: 11, Data: []byte{0x01}},   // not stop runtime
 	}
 	stub, err := argument.Encode(args...)
 	checkError(err)
@@ -36,21 +35,21 @@ func main() {
 	err = os.WriteFile("../asm/inst/argument_x86.inst", []byte(data), 0644)
 	checkError(err)
 
-	cmdlineA = "test_x64.exe -arg 11\x00"
-	cmdlineW = stringToUTF16(cmdlineA)
-
+	cmdline = "test_x64.exe -arg 11"
+	cmdlineA = []byte(cmdline + "\x00")
+	cmdlineW = types.StringToUTF16(cmdline)
 	args = []*argument.Arg{
-		{ID: 1, Data: []byte{0xFF}},     // invalid PE image
-		{ID: 2, Data: []byte(cmdlineA)}, // command line ANSI
-		{ID: 3, Data: []byte(cmdlineW)}, // command line UTF16
-		{ID: 4, Data: make([]byte, 8)},  // standard input handle
-		{ID: 5, Data: make([]byte, 8)},  // standard output handle
-		{ID: 6, Data: make([]byte, 8)},  // standard error handle
-		{ID: 7, Data: []byte{0x01}},     // wait main thread
-		{ID: 8, Data: []byte{0x01}},     // allow skip dll
-		{ID: 9, Data: []byte{0x01}},     // ignore standard handle
-		{ID: 10, Data: []byte{0x01}},    // not auto run
-		{ID: 11, Data: []byte{0x01}},    // not stop runtime
+		{ID: 1, Data: []byte{0xFF}},    // invalid PE image
+		{ID: 2, Data: cmdlineA},        // command line ANSI
+		{ID: 3, Data: cmdlineW},        // command line UTF16
+		{ID: 4, Data: make([]byte, 8)}, // standard input handle
+		{ID: 5, Data: make([]byte, 8)}, // standard output handle
+		{ID: 6, Data: make([]byte, 8)}, // standard error handle
+		{ID: 7, Data: []byte{0x01}},    // wait main thread
+		{ID: 8, Data: []byte{0x01}},    // allow skip dll
+		{ID: 9, Data: []byte{0x01}},    // ignore standard handle
+		{ID: 10, Data: []byte{0x01}},   // not auto run
+		{ID: 11, Data: []byte{0x01}},   // not stop runtime
 	}
 	stub, err = argument.Encode(args...)
 	checkError(err)
@@ -59,15 +58,6 @@ func main() {
 	fmt.Println(data)
 	err = os.WriteFile("../asm/inst/argument_x64.inst", []byte(data), 0644)
 	checkError(err)
-}
-
-func stringToUTF16(s string) string {
-	w := utf16.Encode([]rune(s))
-	output := make([]byte, len(w)*2)
-	for i := 0; i < len(w); i++ {
-		binary.LittleEndian.PutUint16(output[i*2:], w[i])
-	}
-	return string(output)
 }
 
 func dumpBytesHex(b []byte) string {
