0a8db5a0462b5030298db5bc3b06e3773057a76b
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-08-16 14:20:45 +0800

    instance.go: add IgnoreInstOpts to Options, update CreateInstance.

--- loader/instance.go
+++ loader/instance.go
@@ -1,16 +1,17 @@
 package loader
 
 import (
+	"bytes"
 	"embed"
 	"encoding/binary"
 	"errors"
 	"fmt"
 	"io"
 	"strings"
-	"unicode/utf16"
 
 	"github.com/RTS-Framework/GRT-Develop/argument"
-	"github.com/RTS-Framework/GRT-Develop/option"
+	"github.com/RTS-Framework/GRT-Develop/instance"
+	"github.com/RTS-Framework/GRT-Develop/types"
 )
 
 // just for prevent [import _ "embed"] :)
@@ -42,7 +43,7 @@ type Image interface {
 
 // Options contains options about create instance.
 type Options struct {
-	// set the custom loader template.
+	// set the custom loader template, it only lacked argument stub.
 	Template []byte `toml:"template" json:"template"`
 
 	// set the custom image name about the command line prefix.
@@ -50,6 +51,12 @@ type Options struct {
 
 	// set the command line argument about the image.
 	CommandLine string `toml:"cmd_line" json:"cmd_line"`
+
+	// set standard handles for hook GetStdHandle,
+	// if them are NULL, call original GetStdHandle.
+	StdInput  uint64 `toml:"std_input"  json:"std_input"`
+	StdOutput uint64 `toml:"std_output" json:"std_output"`
+	StdError  uint64 `toml:"std_error"  json:"std_error"`
 
 	// wait main thread exit, if it is an exe image.
 	WaitMain bool `toml:"wait_main" json:"wait_main"`
@@ -62,20 +69,17 @@ type Options struct {
 	// If it is true, it will overwrite standard handles.
 	IgnoreStdIO bool `toml:"ignore_stdio" json:"ignore_stdio"`
 
-	// set standard handles for hook GetStdHandle,
-	// if them are NULL, call original GetStdHandle.
-	StdInput  uint64 `toml:"std_input"  json:"std_input"`
-	StdOutput uint64 `toml:"std_output" json:"std_output"`
-	StdError  uint64 `toml:"std_error"  json:"std_error"`
-
 	// not running PE image after load.
 	NotAutoRun bool `toml:"not_auto_run" json:"not_auto_run"`
 
 	// not stop runtime when call ExitProcess.
 	NotStopRuntime bool `toml:"not_stop_runtime" json:"not_stop_runtime"`
 
-	// set Gleam-RT options, usually keep the default value.
-	Runtime option.Options `toml:"runtime" json:"runtime"`
+	// ignore instantiate options.
+	IgnoreInstOpts bool `toml:"ignore_runtime_opts" json:"ignore_runtime_opts"`
+
+	// set instantiate options.
+	Runtime instance.Options `toml:"runtime" json:"runtime"`
 
 	// set additional arguments for upper PE image.
 	// all the ID must greater than 64.
@@ -85,9 +89,6 @@ type Options struct {
 	Stdin  io.Reader `toml:"-" json:"-"`
 	Stdout io.Writer `toml:"-" json:"-"`
 	Stderr io.Writer `toml:"-" json:"-"`
-
-	// if current program is running on GleamRT, must enable it.
-	OnRuntime bool `toml:"-" json:"-"`
 }
 
 // CreateInstance is used to create instance from PE Loader template.
@@ -117,7 +118,7 @@ func CreateInstance(arch string, image Image, opts *Options) ([]byte, error) {
 		imageName += " "
 		cmdLine = imageName + cmdLine
 		cmdLineA = []byte(cmdLine + "\x00")
-		cmdLineW = []byte(stringToUTF16(cmdLine))
+		cmdLineW = types.StringToUTF16(cmdLine)
 	}
 	// process switch about loader config
 	var (
@@ -162,21 +163,29 @@ func CreateInstance(arch string, image Image, opts *Options) ([]byte, error) {
 	if template == nil {
 		template = defaultTemplate
 	}
-	// process runtime options and encode arguments
-	template, err = option.Set(template, &opts.Runtime)
-	if err != nil {
-		return nil, fmt.Errorf("failed to set runtime option: %s", err)
+	// instantiate from template
+	var inst []byte
+	if !opts.IgnoreInstOpts {
+		instOpts := opts.Runtime
+		instOpts.SkipArguments = true
+		inst, err = instance.Instantiate(template, &instOpts)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		inst = bytes.Clone(template)
 	}
+	// encode arguments at tail of instance
 	args := []*argument.Arg{
 		{ID: 1, Data: peImage},
 		{ID: 2, Data: cmdLineA},
 		{ID: 3, Data: cmdLineW},
-		{ID: 4, Data: waitMain},
-		{ID: 5, Data: allowSkipDLL},
-		{ID: 6, Data: ignoreStdIO},
-		{ID: 7, Data: stdInput},
-		{ID: 8, Data: stdOutput},
-		{ID: 9, Data: stdError},
+		{ID: 4, Data: stdInput},
+		{ID: 5, Data: stdOutput},
+		{ID: 6, Data: stdError},
+		{ID: 7, Data: waitMain},
+		{ID: 8, Data: allowSkipDLL},
+		{ID: 9, Data: ignoreStdIO},
 		{ID: 10, Data: notAutoRun},
 		{ID: 11, Data: notStopRuntime},
 	}
@@ -191,14 +200,7 @@ func CreateInstance(arch string, image Image, opts *Options) ([]byte, error) {
 	if err != nil {
 		return nil, fmt.Errorf("failed to encode argument: %s", err)
 	}
-	return append(template, stub...), nil
+	return append(inst, stub...), nil
 }
 
-func stringToUTF16(s string) string {
-	w := utf16.Encode([]rune(s))
-	output := make([]byte, len(w)*2+2)
-	for i := 0; i < len(w); i++ {
-		binary.LittleEndian.PutUint16(output[i*2:], w[i])
-	}
-	return string(output)
-}
+// TODO process command line
