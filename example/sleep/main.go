4e10bf682fcdf0e985360fa76e8b37e56a1ffb3f
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-08-17 13:57:18 +0800

    sleep/main.go: refactor the example about add kernel32.Sleep.

--- example/sleep/main.go
+++ example/sleep/main.go
@@ -6,19 +6,54 @@ import (
 	"strings"
 	"time"
 
+	"golang.org/x/sys/windows"
+
 	"github.com/RTS-Framework/Gleam-RT/runtime"
 )
 
+var (
+	modKernel32 = windows.NewLazyDLL("kernel32.dll")
+	procSleep   = modKernel32.NewProc("Sleep")
+)
+
 func main() {
-	data := strings.Repeat("secret", 1)
+	// for allocate memory in go heap,
+	// not only .text and .data section
+	secret := strings.Repeat("secret", 1)
 
 	for i := 0; i < 3; i++ {
+		do()
+
 		err := gleamrt.Sleep(time.Second)
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Println("sleep complete")
+		fmt.Println("gleamrt.Sleep complete")
 	}
 
-	fmt.Println(data)
+	for i := 0; i < 3; i++ {
+		do()
+
+		kernel32Sleep()
+		fmt.Println("kernel32.Sleep complete")
+	}
+
+	fmt.Println(secret)
+}
+
+func kernel32Sleep() {
+	// trigger Gleam-RT SleepHR
+	fmt.Println("call kernel32.Sleep [hooked]")
+	now := time.Now()
+	ret, _, _ := procSleep.Call(1000)
+	if ret != 0 {
+		log.Fatalf("occurred error when slept: 0x%X\n", ret)
+	}
+	fmt.Println("Sleep:", time.Since(now))
+}
+
+func do() {
+	// plain memory data
+	fmt.Println("simulate running")
+	time.Sleep(3 * time.Second)
 }
