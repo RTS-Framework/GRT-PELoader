c5272454620374dc85cf3e1689733d7b141d857c
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-08-21 13:35:20 +0800

    build.h: redesign the build flag header file about TEST_MODE.

--- test/build.h
+++ test/build.h
@@ -1,21 +1,19 @@
 #ifndef BUILD_H
 #define BUILD_H
 
-// RELEASE_MODE:   remove debug modules for generate shellcode
-// SHELLCODE_MODE: run unit tests under PE Loader shellcode
-// NO_RUNTIME:     not include Gleam-RT for test and debug
+#define TEST_MODE
 
-// #define RELEASE_MODE
-// #define SHELLCODE_MODE
+// ENABLE_DEBUGGER:  enable debugger module for test and debug
+// DISABLE_PIC_MODE: run unit tests under .text instance
+// NO_RUNTIME:       not include Gleam-RT for test and debug
+
+// #define ENABLE_DEBUGGER
+// #define DISABLE_PIC_MODE
 // #define NO_RUNTIME
 
-#ifdef SHELLCODE_MODE
-    #define RELEASE_MODE
-#endif // SHELLCODE_MODE
-
-// disable special warnings for RELEASE_MODE
-#ifdef RELEASE_MODE
-    #pragma warning(disable: 4206)
+// if enable debugger, must disable pic mode
+#ifdef ENABLE_DEBUGGER
+  #define DISABLE_PIC_MODE
 #endif
 
 // disable special warnings for NO_RUNTIME
