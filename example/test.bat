bf3454017302ad5faf570f9d564ec377cd34223e
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-08-21 13:14:57 +0800

    test.bat: move the output binary files to the _test directory.

--- example/test.bat
+++ example/test.bat
@@ -1,16 +1,17 @@
 @echo off
 
-go build -v -trimpath -o GRT-PELoader.exe ../tool/pe_loader/main.go
+go build -v -trimpath -o _test/GRT-PELoader.exe ../tool/pe_loader/main.go
 
-go build -v -trimpath -ldflags "-s -w" -o argument.exe     argument/main.go
-go build -v -trimpath -ldflags "-s -w" -o im_storage.exe   im_storage/main.go
-go build -v -trimpath -ldflags "-s -w" -o sleep.exe        sleep/main.go
-go build -v -trimpath -ldflags "-s -w" -o sleep_raw.exe    sleep_raw/main.go
-go build -v -trimpath -ldflags "-s -w" -o metrics.exe      metrics/main.go
-go build -v -trimpath -ldflags "-s -w" -o watchdog.exe     watchdog/main.go
-go build -v -trimpath -ldflags "-s -w" -o raw_api.exe      raw_api/main.go
-go build -v -trimpath -ldflags "-s -w" -o exit_process.exe exit_process/main.go
+go build -v -trimpath -ldflags "-s -w" -o _test/argument.exe     argument/main.go
+go build -v -trimpath -ldflags "-s -w" -o _test/im_storage.exe   im_storage/main.go
+go build -v -trimpath -ldflags "-s -w" -o _test/sleep.exe        sleep/main.go
+go build -v -trimpath -ldflags "-s -w" -o _test/sleep_raw.exe    sleep_raw/main.go
+go build -v -trimpath -ldflags "-s -w" -o _test/metrics.exe      metrics/main.go
+go build -v -trimpath -ldflags "-s -w" -o _test/watchdog.exe     watchdog/main.go
+go build -v -trimpath -ldflags "-s -w" -o _test/raw_api.exe      raw_api/main.go
+go build -v -trimpath -ldflags "-s -w" -o _test/exit_process.exe exit_process/main.go
 
+cd _test
 GRT-PELoader.exe -pe argument.exe
 GRT-PELoader.exe -pe im_storage.exe
 GRT-PELoader.exe -pe sleep.exe
@@ -19,5 +20,6 @@ GRT-PELoader.exe -pe metrics.exe
 GRT-PELoader.exe -pe watchdog.exe
 GRT-PELoader.exe -pe raw_api.exe
 GRT-PELoader.exe -pe exit_process.exe
+cd ..
 
-del /Q *.exe
+rd /S /Q _test
